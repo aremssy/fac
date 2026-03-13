0fcfd8b6ab81d2aac322393de55d31e460e5359e
Response Architects <responsearchitects@Responses-MacBook-Pro-2.local>
2026-03-13 19:46:35 +0100

    new admin

--- public/build/assets/input-error-CKzB9jnQ.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./ui-C9UzXxo-.js";import{c as o}from"./app-5ab31-X1.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
